543cd61423cb9a981a19696451f8bb4e0817f857
clemsos <clement.renaud@gmail.com>
2013-02-28 15:01:38 +0800

    add localization files

--- public/js/locales.js
+++ public/js/locales.js
@@ -1,13 +1,44 @@
 var _locales = {
-        'zh-cn': {
-
-            "App Name" : "美丽的关联次"
-            
-        },
-
         'en-us': {
 
-            "App Name" : "Keywords & Trends Generator"
+            "app name"      : "Keywords & Trends Generator",
+
+            // Top menu
+            "menu home"         : "Home",
+            "menu clients"      : "Clients",
+            "menu filters"      : "Filter Presets",
+            "menu account"      : "Account",
+            "menu logout"       : "Logout",
+
+            // Filters
+            "filter both"       : "Both",
+            "filter men"        : "Men",
+            "filter women"      : "Women",
+
+            // etc.
+
+
+        },
+
+        'zh-cn': {
+
+            "app name" : "美丽的关联次",
+
+            // Top menu
+            "menu home"     : "首页",
+            "menu clients"  : "",
+            "menu filters"      : "Filter Presets",
+            "menu account"      : "Account",
+            "menu logout"       : "Logout",
+
+
+            // Filters
+            "filter both"       : "两",
+            "filter men"        : "男",
+            "filter women"      : "女",
+
+
+        
          
         }
     }
