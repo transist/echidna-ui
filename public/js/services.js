23243859e13ceb6a70731ef31e3546e5c42ab553
Ricky Ng-Adam <rngadam@gmail.com>
2013-03-22 20:27:54 +0800

    clean and document

--- public/js/services.js
+++ public/js/services.js
@@ -1,6 +1,8 @@
 /* Services */
 // console.log(window);
 
+// return browserified libraries binded to the global scope
+// see echidna-data / bundle.js and app.js
 app.factory('feedconfig', function($window) {
     
     // return new ApiClient();
@@ -13,8 +15,6 @@ app.factory('d3data', function($window) {
     return new $window.d3container.D3Container();
 
 });
-
-
 
 app.factory('socket', function ($rootScope) {
     var socket = io.connect();
@@ -46,61 +46,3 @@ app.factory('socket', function ($rootScope) {
 
     };
   });
-
-
-
-function ApiClient() {
-
-    var apiClient = this;
-
-    // Default state    
-    apiClient.init = false;
-
-    // Default values     
-    apiClient.feedConfig = {
-
-        gender      : "Both",      // "Both", "Men", "Women"
-        age         : "All",       // "All", "18-", "24-", "35-", "40+"              
-        tier        : "All",       // "All", "Tier1", "Tier2", "Tier3"               
-        // city     :  null  ;      // "pinyin" (pinyin formatted name of a city)  Optional
-
-        start       : "", // date formatted 
-        end         : "", // date formatted 
-        sampling    : "second",
-
-        samples     : 5 // number of keywords
-        
-    };
-
-}
-
-
-//  http://www.redips.net/javascript/array-move/
-Array.prototype.move = function (pos1, pos2) {
-    // local variables
-    var i, tmp;
-    // cast input parameters to integers
-    pos1 = parseInt(pos1, 10);
-    pos2 = parseInt(pos2, 10);
-    // if positions are different and inside array
-    if (pos1 !== pos2 &&
-        0 <= pos1 && pos1 <= this.length &&
-        0 <= pos2 && pos2 <= this.length) {
-        // save element from position 1
-        tmp = this[pos1];
-        // move element down and shift other elements up
-        if (pos1 < pos2) {
-            for (i = pos1; i < pos2; i++) {
-                this[i] = this[i + 1];
-            }
-        }
-        // move element up and shift other elements down
-        else {
-            for (i = pos1; i > pos2; i--) {
-                this[i] = this[i - 1];
-            }
-        }
-        // put element from position 1 to destination
-        this[pos2] = tmp;
-    }
-}
